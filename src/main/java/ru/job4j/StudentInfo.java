cb354a266e85a0a5f07fa9babe4ec38e59b5fc82
Konstantin K <123@mail.ru>
2022-03-19 18:09:35 +0300

    Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Kuzminykh Konstantin");
+        System.out.println("07.05.1985");
+
+    }
+}
